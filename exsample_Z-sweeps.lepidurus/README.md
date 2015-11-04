8418d1577cde6b9e0ed34e0632b488037c5506ed
andersfi <anders.finstad@ntnu.no>
2015-11-04 13:17:37 +0100

    changed dwc usage

--- exsample_Z-sweeps.lepidurus/README.md
+++ exsample_Z-sweeps.lepidurus/README.md
@@ -1,10 +1,10 @@
-# Survey of Lepidurus arcticus occurence and relative abundance (Z-sweeps) on North-east Greenland
+# Survey of Lepidurus arcticus occurrence and relative abundance (Z-sweeps) on North-east Greenland
 
-Trying to use the event core extention of Darwin Core Archives for documenting cases of abundance type data where there are several sampling instances that leaves blank sampels (i.e. negative obgservations). 
+The data set origins from a small survey of Lepidurus Arcticus in the Zackenberg area North-east Greenland. It is presented as an example of using the event core extension of Darwin Core Archives for documenting the sampling process. Although the basic data is occurrence type (i.e. answer the question where did we find L. Arcticus in the area sampled), there are also invaluable information in the data that would be lost if not the sampling process is documented. Particularly, this involves the abundance type of data (semiquanitative CPUE given as catches per unit effort) and negative observations: i.e. there are several sampling instances that leaves blank samples (i.e. negative observations). For the latter, there is presently no good way of explicitly documenting this in Darwin core. The field OccurenceStatus having the option of indicating "absence" does not capture that other species may have been looked for and not found. In this case we have been screening the data also for other Notostraca species (although we are pretty sure they don't exist in the area). This is indicated in the eventRemarks field as "eventTaxonomicRange:Notostraca spp.". A suggestion could be to include something like "eventTaxonomicRange" in the event core to solve this issue. 
 
-Folder contains raw data (as punced from field forms) in xls format [(or as google spreadsheet or .csv  here)](https://goo.gl/z25YXj), R-scripts to convert these data to dwca format and text (csv) files with the formated data ready to upload into the IPT. 
+The folder contains raw data (as punched from field forms) in xls format, R-scripts to convert these data to Darwin Core Archive event core format, and text (csv) files with the formatted data ready to upload into the IPT. 
 
 Resource URL: http://gbif.vm.ntnu.no/ipt/resource?r=lepidurus-arcticus-survey_northeast-greenland_2013&v=1.1
 
-Raw data innput on google docs: https://docs.google.com/spreadsheets/d/1AkZSzhSqf5LWdc0Vos8BXkCP2egeD6wp-Tgh1Dwho1A/edit?usp=sharing
+Raw data input on google docs: https://docs.google.com/spreadsheets/d/1AkZSzhSqf5LWdc0Vos8BXkCP2egeD6wp-Tgh1Dwho1A/edit?usp=sharing
 
--- exsample_Z-sweeps.lepidurus/dwca_example_z-sweeps.R
+++ exsample_Z-sweeps.lepidurus/dwca_example_z-sweeps.R
@@ -13,7 +13,7 @@ library(xlsx)
 library(tidyr)
 library(dplyr)
 
-# set directory of directory with data if not in same directory as the script
+# set working directory if your data is not in the same directory as the script
 setwd("C:\\iSkya\\GitHub\\andersfi\\DwC\\exsample_Z-sweeps.lepidurus")
 
 #######################################################################
@@ -30,24 +30,26 @@ setwd("C:\\iSkya\\GitHub\\andersfi\\DwC\\exsample_Z-sweeps.lepidurus")
 xl.inn <- "Lepidurus_Zackenberg.xls"
 # read data
 
-occurrence <- read.xlsx2(file=xl.inn,sheetName="occurrence")
-event <- read.xlsx2(file=xl.inn, sheetName="event")
-location <- read.xlsx2(file=xl.inn,sheetName="location")
-measurm_facts <- read.xlsx2(file=xl.inn,sheetName="measurment_and_facts")
-measurm_description <- read.xlsx2(file=xl.inn,sheetName="measurments_description")
+occurrence <- read.xlsx2(file=xl.inn,sheetName="occurrence") # the sheet containing the occurence information (i.e. organisms sampled during the event)
+event <- read.xlsx2(file=xl.inn, sheetName="event") # the sheet with the information about the sampling events
+location <- read.xlsx2(file=xl.inn,sheetName="location") # the sheet with the information about the location
+measurm_facts <- read.xlsx2(file=xl.inn,sheetName="measurment_and_facts") # measurements and facts: in this case location specific information, but could be also specific information about a given sampling event 
+measurm_description <- read.xlsx2(file=xl.inn,sheetName="measurments_description") # description of the terms used in the measurment and facts sheet
+DwC_remarks <- read.xlsx2(file=xl.inn,sheetName="DwC_remarks") # remarks about the specific use of Darwin Core terms in this dataset 
 
-##################################################
-# merge the location and event table
+
+#####################################################################
+# The data for the event core is for convinience punched in two tables
+# Here, we merge the location and event table
 # into one table (i.e. the event core of a DwC Archive)
-##################################################
+#######################################################################
 
 # first join event and occurence table 
   event_CORE <- left_join(event, location)
 
-# the date column is no good (xl store dates as numbers), 
-# use the day,month,year columns to create date in ISO format
+# Prefere to use day,month,year format for dates since this is an XL based dataset (xl store dates as numbers), 
+# Here, we use the day,month,year columns to create date in ISO format
 eventDate <- paste(event_CORE$year,event_CORE$month,event_CORE$day,sep="-")
-event_CORE <- subset(event_CORE, select = -c(date) )
 
 # write output to workdir
 write.csv(event_CORE, "event_CORE.csv",row.names=F)
@@ -87,32 +89,26 @@ write.csv(measurements_and_facts_EXTENTION, "measurements.extention.csv",row.nam
 
 
 #######################################################################
+# 
+# Write out hte DwC term description sheet as csv table for documentation 
+#
+#####################################################################
+
+write.table(DwC_remarks, "DwC_remarks.csv",row.names=F,sep=";")
+
+
+#######################################################################
 #
 # Testcase - take data back from GBIF using the rgbif library
 #
+# under construction...... 
+# 
 #####################################################################
 library(rgbif)
 
-
-
-test <- occ_search(datasetKey = "78360224-5493-45fd-a9a0-c336557f09c3",limit=300,return="all")
-lepidata <- test$data  
-gbifmap(test)
-           , 
-           fields=c('name','basisOfRecord','protocol'), limit = 20)
-
-lepidurus <- datasets(uuid="78360224-5493-45fd-a9a0-c336557f09c3")
-
-test <- occ_download("datasetKey-78360224-5493-45fd-a9a0-c336557f09c3")
-
-key <- name_backbone(name='Puma concolor')$speciesKey
-dat <- occ_search(taxonKey=key, return='data', limit=300)
-gbifmap(dat)
-
-key <- name_backbone(name='Puma concolor')$speciesKey
-dat <- occ_search(taxonKey=key, return='data', limit=300)
-gbifmap(dat)
-
-
-
-
+test <- occ_search(datasetKey = "78360224-5493-45fd-a9a0-c336557f09c3",limit=300,return="data")
+head(test)
+str(test)
+dim(test)
+    
+# sligth problem - the search only returns the occurences, how to retrive all events - also those without occurences?        
